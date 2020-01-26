98868ee6d42310e579a175d3a24b952e90cf006c
Paul <pfreema1@gmail.com>
2020-01-26 14:04:22 -0500

    16th notes nah

--- src/scripts/audio/Audio.js
+++ src/scripts/audio/Audio.js
@@ -15,50 +15,55 @@ export default class Audio {
         this.initTone();
     }
 
-    initTone() {
-        const synths = [
+    initSynths() {
+        this.synths = [
             new Tone.Synth(),
             new Tone.Synth(),
             new Tone.Synth()
         ];
 
-        synths[0].oscillator.type = 'triangle';
-        synths[1].oscillator.type = 'sine';
-        synths[2].oscillator.type = 'sawtooth';
+        this.synths[0].oscillator.type = 'triangle';
+        this.synths[1].oscillator.type = 'sine';
+        this.synths[2].oscillator.type = 'sawtooth';
 
-        const gain = new Tone.Gain(0.6);
-        gain.toMaster();
+        this.gain = new Tone.Gain(0.6);
+        this.gain.toMaster();
 
-        synths.forEach(synth => synth.connect(gain));
+        this.synths.forEach(synth => synth.connect(this.gain));
+    }
 
-        const $rows = document.querySelectorAll('.sequencer-row-container');
-        const notes = ['G5', 'E4', 'C3'];
-        let index = 0;
+    initTone() {
 
-        Tone.Transport.scheduleRepeat(repeat.bind(this), '8n');
+        this.initSynths();
+
+        this.$rows = document.querySelectorAll('.sequencer-row-container');
+        this.notes = ['G5', 'E4', 'C3'];
+        this.index = 0;
+
+        Tone.Transport.scheduleRepeat(this.handleBeat.bind(this), '16n');
         Tone.Transport.start();
 
-        function repeat(time) {
-            let step = index % 8;
-            for (let i = 0; i < $rows.length; i++) {
-                let synth = synths[i],
-                    note = notes[i],
-                    $row = $rows[i],
-                    $input = $row.querySelector(`input:nth-child(${step + 1})`);
+    }
 
-                console.log($input.checked);
+    handleBeat(time) {
+        let step = this.index % 16;
 
-                if ($input.checked) {
-                    // console.log(this.webGLView);
-                    if (this.webGLView) {
+        for (let i = 0; i < this.$rows.length; i++) {
+            let synth = this.synths[i],
+                note = this.notes[i],
+                $row = this.$rows[i],
+                $input = $row.querySelector(`input:nth-child(${step + 1})`);
 
-                        this.webGLView.lines.triggerSpin();
-                    }
-                    synth.triggerAttackRelease(note, '8n', time);
+
+            if ($input.checked) {
+                if (this.webGLView) {
+                    this.webGLView.lines.triggerSpin();
                 }
+
+                synth.triggerAttackRelease(note, '16n', time);
             }
-            index++;
         }
+        this.index++;
     }
 
     userGestureFix() {
